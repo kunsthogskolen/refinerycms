5a52f1970ddac6985047dd6c468d600b4f8b2a7c
Peter Kordel <pkordel@gmail.com>
2018-02-06 11:27:52 +0700

    Cleanup

--- pages/app/models/refinery/page.rb
+++ pages/app/models/refinery/page.rb
@@ -58,10 +58,6 @@ module Refinery
 
     accepts_nested_attributes_for :parts, :allow_destroy => true
 
-    # before_save do |m|
-    #   m.translation.globalized_model = self
-    #   m.translation.save if m.translation.new_record?
-    # end
     before_create :ensure_locale!
     before_destroy :deletable?
     after_save :reposition_parts!
