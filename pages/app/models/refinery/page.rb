ac17b4d9527dfed357c0dc80990e95bba93c7025
Peter Kordel <pkordel@gmail.com>
2020-04-21 14:15:00 +0700

    Revert linting

--- pages/app/models/refinery/page.rb
+++ pages/app/models/refinery/page.rb
@@ -1,13 +1,14 @@
+# Encoding: utf-8
 require 'friendly_id'
 require 'refinery/core/base_model'
 require 'refinery/pages/url'
 require 'refinery/pages/finder'
 
 module Refinery
-  class Page < Core::BaseModel # rubocop:disable Metrics/ClassLength
+  class Page < Core::BaseModel
     extend FriendlyId
 
-    translates :title, :menu_title, :custom_slug, :slug, include: :seo_meta
+    translates :title, :menu_title, :custom_slug, :slug, :include => :seo_meta
 
     attribute :title
     attribute :menu_title
@@ -20,7 +21,7 @@ module Refinery
       is_seo_meta
 
       def self.seo_fields
-        ::SeoMeta.attributes.keys.map { |a| [a, :"#{a}="] }.flatten
+        ::SeoMeta.attributes.keys.map{ |a| [a, :"#{a}="]}.flatten
       end
     end
 
@@ -33,7 +34,7 @@ module Refinery
         }
         if ::Refinery::Pages.scope_slug_by_parent
           friendly_id_options[:use] << :scoped
-          friendly_id_options[:scope] = :parent
+          friendly_id_options.merge!(scope: :parent)
         end
         friendly_id_options[:use] << :globalize
         friendly_id_options
@@ -42,34 +43,32 @@ module Refinery
 
     # If title changes tell friendly_id to regenerate slug when saving record
     def should_generate_new_friendly_id?
-      changes.keys?("title") || changes.keys?("custom_slug")
+      changes.keys.include?("title") || changes.keys.include?("custom_slug")
     end
 
     # Delegate SEO Attributes to globalize translation
-    delegate(*(Translation.seo_fields << { to: :translation }))
+    delegate(*(Translation.seo_fields << {:to => :translation}))
 
-    validates :title, presence: true
+    validates :title, :presence => true
 
-    validates :custom_slug, uniqueness: true, allow_blank: true
+    validates :custom_slug, :uniqueness => true, :allow_blank => true
 
     # Docs for acts_as_nested_set https://github.com/collectiveidea/awesome_nested_set
     # rather than :delete_all we want :destroy
-    acts_as_nested_set(counter_cache: :children_count, dependent: :destroy, touch: false)
+    acts_as_nested_set counter_cache: :children_count, dependent: :destroy, touch: false
 
     friendly_id :custom_slug_or_title, FriendlyIdOptions.options
 
-    has_many :parts, lambda {
+    has_many :parts, -> {
       scope = order('position ASC')
-      if ::Refinery::PagePart.respond_to?(:translation_class)
-        scope = scope.includes(:translations)
-      end
+      scope = scope.includes(:translations) if ::Refinery::PagePart.respond_to?(:translation_class)
       scope
-    },       foreign_key: :refinery_page_id,
-             class_name: '::Refinery::PagePart',
-             inverse_of: :page,
-             dependent: :destroy
+    },       :foreign_key => :refinery_page_id,
+             :class_name => '::Refinery::PagePart',
+             :inverse_of => :page,
+             :dependent => :destroy
 
-    accepts_nested_attributes_for :parts, allow_destroy: true
+    accepts_nested_attributes_for :parts, :allow_destroy => true
 
     before_destroy :deletable?
     after_save :reposition_parts!
@@ -81,7 +80,7 @@ module Refinery
       # Live pages are 'allowed' to be shown in the frontend of your website.
       # By default, this is all pages that are not set as 'draft'.
       def live
-        where(draft: false)
+        where(:draft => false)
       end
 
       # Find page by path, checking for scoping rules
@@ -102,7 +101,7 @@ module Refinery
       #
       # raise ActiveRecord::RecordNotFound if not found.
       def find_by_path_or_id!(path, id)
-        page = find_by(path: path, id: id)
+        page = find_by_path_or_id(path, id)
 
         raise ActiveRecord::RecordNotFound unless page
 
@@ -130,7 +129,7 @@ module Refinery
       # This works using a query against the translated content first and then
       # using all of the page_ids we further filter against this model's table.
       def in_menu
-        where(show_in_menu: true).with_globalize
+        where(:show_in_menu => true).with_globalize
       end
 
       # An optimised scope containing only live pages ordered for display in a menu.
@@ -155,13 +154,11 @@ module Refinery
 
       protected
 
-      def nullify_duplicate_slugs_under_the_same_parent! # rubocop:disable Metrics/AbcSize
+      def nullify_duplicate_slugs_under_the_same_parent!
         t_slug = translation_class.arel_table[:slug]
-        joins(:translations).group(:locale, :parent_id, t_slug)
-                            .having(t_slug.count.gt(1)).count
-                            .each do |(locale, parent_id, slug), _count|
-          by_slug(slug, locale: locale).where(parent_id: parent_id)
-                                       .drop(1).each do |page|
+        joins(:translations).group(:locale, :parent_id, t_slug).having(t_slug.count.gt(1)).count.
+        each do |(locale, parent_id, slug), count|
+          by_slug(slug, :locale => locale).where(:parent_id => parent_id).drop(1).each do |page|
             page.slug = nil # kill the duplicate slug
             page.save # regenerate the slug
           end
@@ -170,9 +167,7 @@ module Refinery
     end
 
     def translated_to_default_locale?
-      persisted? && translations.any? do |t|
-        t.locale == Refinery::I18n.default_frontend_locale
-      end
+      persisted? && translations.any?{ |t| t.locale == Refinery::I18n.default_frontend_locale}
     end
 
     # The canonical page for this particular page.
@@ -196,10 +191,8 @@ module Refinery
     end
 
     # Am I allowed to delete this page?
-    # If a link_url is set we don't want to break the link so we don't allow them
-    # to delete
-    # If deletable is set to false then we don't allow this page to be deleted.
-    # These are often Refinery system pages
+    # If a link_url is set we don't want to break the link so we don't allow them to delete
+    # If deletable is set to false then we don't allow this page to be deleted. These are often Refinery system pages
     def deletable?
       deletable && link_url.blank? && menu_match.blank?
     end
@@ -208,7 +201,7 @@ module Refinery
     # This ensures that they are in the correct 0,1,2,3,4... etc order.
     def reposition_parts!
       reload.parts.each_with_index do |part, index|
-        part.update_columns position: index # rubocop:disable Rails/SkipsModelValidations
+        part.update_columns position: index
       end
     end
 
@@ -222,12 +215,11 @@ module Refinery
       false
     end
 
-    # If you want to destroy a page that is set to be not deletable this is the way
-    # to do it.
+    # If you want to destroy a page that is set to be not deletable this is the way to do it.
     def destroy!
-      update(menu_match: nil, link_url: nil, deletable: true)
+      self.update_attributes(:menu_match => nil, :link_url => nil, :deletable => true)
 
-      destroy
+      self.destroy
     end
 
     # Returns the full path to this page.
@@ -260,7 +252,7 @@ module Refinery
     #
     #   ['about', 'mission']
     #
-    alias uncached_nested_url nested_url
+    alias_method :uncached_nested_url, :nested_url
 
     # Returns the string version of nested_url, i.e., the path that should be
     # generated by the router
@@ -290,15 +282,15 @@ module Refinery
 
     def to_refinery_menu_item
       {
-        id: id,
-        lft: lft,
-        depth: depth,
-        menu_match: menu_match,
-        parent_id: parent_id,
-        rgt: rgt,
-        title: menu_title.presence || title.presence,
-        type: self.class.name,
-        url: url
+        :id => id,
+        :lft => lft,
+        :depth => depth,
+        :menu_match => menu_match,
+        :parent_id => parent_id,
+        :rgt => rgt,
+        :title => menu_title.presence || title.presence,
+        :type => self.class.name,
+        :url => url
       }
     end
 
@@ -332,7 +324,7 @@ module Refinery
     def part_with_slug(part_slug)
       # self.parts is usually already eager loaded so we can now just grab
       # the first element matching the title we specified.
-      parts.detect do |part|
+      self.parts.detect do |part|
         part.slug_matches?(part_slug)
       end
     end
@@ -357,23 +349,22 @@ module Refinery
           .sub(%r{/*$}, '')
           .split('/')
           .select(&:present?)
-          .map { |slug| normalize_friendly_id(slug) }.join('/')
+          .map { |slug| self.normalize_friendly_id(slug) }.join('/')
       end
 
       def self.normalize_friendly_id(slug_string)
         # If we are scoping by parent, no slashes are allowed. Otherwise, slug is
         # potentially a custom slug that contains a custom route to the page.
         if !Pages.scope_slug_by_parent && slug_string.include?('/')
-          normalize_friendly_id_path(slug_string)
+          self.normalize_friendly_id_path(slug_string)
         else
-          protected_slug_string(slug_string)
+          self.protected_slug_string(slug_string)
         end
       end
 
       def self.protected_slug_string(slug_string)
         sluggified = slug_string.to_slug.normalize!
-        if Pages.marketable_urls &&
-           Refinery::Pages.friendly_id_reserved_words.include?(sluggified)
+        if Pages.marketable_urls && Refinery::Pages.friendly_id_reserved_words.include?(sluggified)
           sluggified << "-page"
         end
         sluggified
@@ -381,21 +372,16 @@ module Refinery
     end
 
     def puts_destroy_help
-      puts "This page is not deletable. Please use .destroy! if you really want " \
-           "it deleted "
+      puts "This page is not deletable. Please use .destroy! if you really want it deleted "
       puts "unset .link_url," if link_url.present?
       puts "unset .menu_match," if menu_match.present?
       puts "set .deletable to true" unless deletable
     end
 
-    def slug_locale # rubocop:disable Metrics/AbcSize
-      if translation_for(Globalize.locale, false).try(:slug).present?
-        return Globalize.locale
-      end
+    def slug_locale
+      return Globalize.locale if translation_for(Globalize.locale, false).try(:slug).present?
 
-      if translations.empty? || translation_for(
-        Refinery::I18n.default_frontend_locale, false
-      ).try(:slug).present?
+      if translations.empty? || translation_for(Refinery::I18n.default_frontend_locale, false).try(:slug).present?
         Refinery::I18n.default_frontend_locale
       else
         translations.first.locale
@@ -403,7 +389,7 @@ module Refinery
     end
 
     def update_all_descendants
-      descendants.map(&:touch)
+      self.descendants.map(&:touch)
     end
   end
 end
--- pages/app/models/refinery/page_part.rb
+++ pages/app/models/refinery/page_part.rb
@@ -1,11 +1,12 @@
 module Refinery
   class PagePart < Refinery::Core::BaseModel
-    belongs_to :page, foreign_key: :refinery_page_id, touch: true
+
+    belongs_to :page, :foreign_key => :refinery_page_id, :touch => true
 
     before_validation :set_default_slug
 
-    validates :title, presence: true
-    validates :slug, presence: true, uniqueness: { scope: :refinery_page_id }
+    validates :title, :presence => true
+    validates :slug, :presence => true, :uniqueness => {:scope => :refinery_page_id}
     alias_attribute :content, :body
 
     translates :body
@@ -23,7 +24,7 @@ module Refinery
     end
 
     def slug_matches?(other_slug)
-      slug.present? && (
+      slug.present? && (# protecting against the problem that occurs when have two nil slugs
         slug == other_slug.to_s ||
         parameterized_slug == parameterize(other_slug.to_s)
       )
@@ -32,18 +33,14 @@ module Refinery
     protected
 
     def normalise_text_fields
-      return unless read_attribute(:body).present? && read_attribute(:body) !~ /^</
-
-      write_attribute(
-        :body, "<p>#{read_attribute(:body).gsub("\r\n\r\n", '</p><p>')
-                                          .gsub("\r\n", '<br/>')}</p>"
-      )
+      if read_attribute(:body).present? && read_attribute(:body) !~ %r{^<}
+        write_attribute(:body, "<p>#{read_attribute(:body).gsub("\r\n\r\n", "</p><p>").gsub("\r\n", "<br/>")}</p>")
+      end
     end
 
     private
-
     def parameterize(string)
-      string.downcase.tr(" ", "_")
+      string.downcase.gsub(" ", "_")
     end
 
     def parameterized_slug
@@ -51,7 +48,7 @@ module Refinery
     end
 
     def set_default_slug
-      self.slug = title.to_s.parameterize.underscore.presence if slug.blank?
+      self.slug = title.to_s.parameterize.underscore.presence if self.slug.blank?
     end
   end
 end
